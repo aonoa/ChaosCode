c5e0230078a5a6fc197b5096703088d94fd7624e
aonoa <32682251+aonoa@users.noreply.github.com>
2019-01-22 11:00:28 +0800

    Create chaos.c
    比较3个整数大小

---
+++ chaos.c
@@ -0,0 +1,2 @@
+#include <stdio.h>
+int main(int _, int i, int *j){return 0 < _ ? printf("%d", (scanf("%d%d", &i, &_), main(0, main(0, i, &_), &_))) : i > *j ? _ : (i = i + *j, *j = i - *j, i = i - *j), scanf("%d", j), ungetc('\\', stdin), i;}
